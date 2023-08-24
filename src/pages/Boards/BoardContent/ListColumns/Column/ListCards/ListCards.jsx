ab203d918732bbec24bef771908211b9667d80de
Trummm <trungpro2000.tp2@gmail.com>
2023-08-24 16:22:22 +0700

    drag_overlay

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,31 +1,40 @@
 import { Box } from '@mui/material'
 import Card from './Card/Card'
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
 
 const COLUMN_HEADER_HEIGHT = '50px'
 const COLUMN_FOOTER_HEIGHT = '56px'
 
 function ListCards({ cards }) {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 1,
-        p: '0 5px',
-        m: '0 5px',
-        overflowX: 'hidden',
-        overflowY: 'auto',
-        maxHeight: (theme) =>
-          `calc(${theme.trello.boardContentHeight} - ${theme.spacing(
-            5
-          )} - ${COLUMN_HEADER_HEIGHT} - ${COLUMN_FOOTER_HEIGHT})`,
-        '&::-webkit-scrollbar': { display: 'none' },
-      }}
+    <SortableContext
+      items={cards.map((item) => item._id)}
+      strategy={verticalListSortingStrategy}
     >
-      {cards.map((card) => {
-        return <Card key={card._id} card={card} />
-      })}
-    </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          p: '0 5px',
+          m: '0 5px',
+          overflowX: 'hidden',
+          overflowY: 'auto',
+          maxHeight: (theme) =>
+            `calc(${theme.trello.boardContentHeight} - ${theme.spacing(
+              5
+            )} - ${COLUMN_HEADER_HEIGHT} - ${COLUMN_FOOTER_HEIGHT})`,
+          '&::-webkit-scrollbar': { display: 'none' },
+        }}
+      >
+        {cards.map((card) => {
+          return <Card key={card._id} card={card} />
+        })}
+      </Box>
+    </SortableContext>
   )
 }
 
